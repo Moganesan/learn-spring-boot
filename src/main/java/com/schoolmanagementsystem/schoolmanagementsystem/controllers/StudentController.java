9d72f775677f849288893fc50edbc75a40fb30ec
moganesan <developer.moganesan>
2023-07-05 17:49:17 +0530

    added endpoints for adding and retrieving students

--- src/main/java/com/schoolmanagementsystem/schoolmanagementsystem/controllers/StudentController.java
+++ src/main/java/com/schoolmanagementsystem/schoolmanagementsystem/controllers/StudentController.java
@@ -2,7 +2,6 @@ package com.schoolmanagementsystem.schoolmanagementsystem.controllers;
 
 import com.schoolmanagementsystem.schoolmanagementsystem.models.Student;
 import com.schoolmanagementsystem.schoolmanagementsystem.repository.StudentRepository;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
