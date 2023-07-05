644c3fda5ea062c582a1b033f2a1fa429635b66e
moganesan <developer.moganesan>
2023-07-05 18:21:20 +0530

    simple CRUD API completed

--- src/main/java/com/schoolmanagementsystem/schoolmanagementsystem/controllers/StudentController.java
+++ src/main/java/com/schoolmanagementsystem/schoolmanagementsystem/controllers/StudentController.java
@@ -1,5 +1,6 @@
 package com.schoolmanagementsystem.schoolmanagementsystem.controllers;
 
+import com.schoolmanagementsystem.schoolmanagementsystem.exceptions.ResourceNotFoundException;
 import com.schoolmanagementsystem.schoolmanagementsystem.models.Student;
 import com.schoolmanagementsystem.schoolmanagementsystem.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.rmi.StubNotFoundException;
 import java.util.Optional;
 
 
@@ -32,6 +34,25 @@ public class StudentController {
         return ResponseEntity.ok(student);
     }
 
+    @PatchMapping("/{studentId}")
+    public ResponseEntity<?> updateUser(@PathVariable Long studentId,@RequestBody Student updatedStudent){
+
+            Student student = studentRepository.findById(studentId).orElseThrow(() -> new ResourceNotFoundException("Student not found with id: " + studentId));
+            student.setName(updatedStudent.getName());
+            student.setAge(updatedStudent.getAge());
+            student.setRollNo(updatedStudent.getRollNo());
+            Student savedStudent = studentRepository.save(student);
+            return ResponseEntity.ok("Student Details Updated");
+    }
+
+
+    @DeleteMapping("/{studentId}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long studentId){
+
+    Student student =    studentRepository.findById(studentId).orElseThrow(() -> new ResourceNotFoundException("Student not found with id: " + studentId));
+    studentRepository.deleteById(student.getId());
+        return ResponseEntity.ok("Student Deleted Successfully.");
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception ex) {
--- src/main/java/com/schoolmanagementsystem/schoolmanagementsystem/models/Student.java
+++ src/main/java/com/schoolmanagementsystem/schoolmanagementsystem/models/Student.java
@@ -6,18 +6,21 @@ import jakarta.persistence.*;
 @Table(name = "students")
 public class Student {
     @Id
-    private int id;
+    private Long id;
 
     private String name;
     private Integer age;
     private Integer rollNo;
 
-    public Integer getId(){
+
+    public Long getId() {
         return id;
     }
-    public void setId(Integer id){
+
+    public void setId(Long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
